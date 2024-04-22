4fe199e7be4bcd7588d88ac1f7e287fb8895fb1c
Teinturier <henri.teinturier@gmail.com>
2024-04-22 11:42:46 +0200

    chapter 4 handleError with useFormState in action server

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,20 +7,42 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({ invalid_type_error: 'Please select a customer.' }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter a amount greater than 0$' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select a status.',
+  }),
   date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+
+  const { customerId, amount, status } = validatedFields.data;
   // It's usually good practice to store monetary values in cents in your database to eliminate JavaScript floating-point errors and ensure greater accuracy.
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
@@ -68,7 +90,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error('Failed to delete invoice.');
+  // throw new Error('Failed to delete invoice.');
 
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
--- app/ui/button.tsx
+++ app/ui/button.tsx
@@ -4,16 +4,29 @@ interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
-export function Button({ children, className, ...rest }: ButtonProps) {
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  loading?: boolean;
+  className?: string;
+}
+
+export function Button({ children, className, loading, ...rest }: ButtonProps) {
   return (
     <button
       {...rest}
+      disabled={rest.disabled || loading}
       className={clsx(
         'flex h-10 items-center rounded-lg bg-blue-500 px-4 text-sm font-medium text-white transition-colors hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-500 active:bg-blue-600 aria-disabled:cursor-not-allowed aria-disabled:opacity-50',
+        {
+          'cursor-not-allowed opacity-50': loading,
+        },
         className,
       )}
     >
-      {children}
+      {loading ? (
+        <span>Loading...</span> // Placeholder for a spinner or any loading animation
+      ) : (
+        children
+      )}
     </button>
   );
 }
