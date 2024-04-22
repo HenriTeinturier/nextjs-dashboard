4c298e10360e29d43cf8cfcfbd857a5c93728390
Teinturier <henri.teinturier@gmail.com>
2024-04-22 10:49:03 +0200

    chapter 13 handle error

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -147,6 +147,7 @@ export async function fetchInvoicesPages(query: string) {
 }
 
 export async function fetchInvoiceById(id: string) {
+  noStore();
   try {
     const data = await sql<InvoiceForm>`
       SELECT
@@ -163,7 +164,6 @@ export async function fetchInvoiceById(id: string) {
       // Convert amount from cents to dollars
       amount: invoice.amount / 100,
     }));
-
     return invoice[0];
   } catch (error) {
     console.error('Database Error:', error);
