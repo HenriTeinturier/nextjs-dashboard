6a94cba804438dafdd0f045726d74cb5bf858bbf
Teinturier <henri.teinturier@gmail.com>
2024-04-22 13:22:16 +0200

    add url in oepnGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,15 @@ import { inter } from '@/app/ui/fonts';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: 'Acme Dashboard',
-  description: 'The official Next.js Course Dashboard, built with App Router.',
+  title: 'Title: Acme Dashboard',
+  description:
+    'Description: The official Next.js Course Dashboard, built with App Router.',
   metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://next-learn-dashboard.vercel.sh',
+  },
 };
 
 export default function RootLayout({
